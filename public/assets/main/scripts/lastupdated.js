a8b5ba669e06397655c60b09e71271bfe3ca6ae9
Linkhe <kikokvo83@gmail.com>
2024-10-29 22:46:40 -0300

    reworked website

--- public/assets/main/scripts/lastupdated.js
+++ public/assets/main/scripts/lastupdated.js
@@ -66,7 +66,7 @@ function formatarData(valor) {
 
   // Formatando a string
   // return "Last Updated: " + mes + " " + dia + ", " + partes[1] + " - " + hora + ":" + partes[5] + manhanoite +" BRT";
-  return "Last Updated: " + mes + " " + dia + ", " + partes[1] + " - " + hora + ":" + partes[5] +" BRT";
+  return mes + " " + dia + ", " + partes[1] + " - " + hora + ":" + partes[5] +" BRT";
 }
 
 // Chamando a função lerArquivoJSON para ler o arquivo e encontrar a chave
